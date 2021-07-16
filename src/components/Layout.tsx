6298166212c7763847c907b9a2cec7a01220b3c1
Resat Kubilay Kandemir <kubilaykandemir@outlook.com>
2021-07-17 02:47:41 +1000

    Some cleaning in scss files.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import Navbar from './Navbar';
-import '../styles/global.scss';
+import React from "react";
+import Navbar from "./Navbar";
+import "../styles/global.scss";
 
 const Layout: React.FC = ({ children }) => {
   return (
     <div>
       <Navbar />
-      <h1>layout</h1>
       {children}
     </div>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,44 +4,46 @@ import { Link } from "gatsby";
 
 const Navbar: React.FC = () => {
   return (
-    <nav>
+    <header>
       <div className="title_container">
         <Link to="/">Kubilay Kandemir</Link>
       </div>
-      <ul className="inside_links">
-        <li>
-          <Link to="/">Homepage</Link>
-        </li>
-        <li>
-          <Link to="/tags">Tags</Link>
-        </li>
-        <li>
-          <Link to="about">About</Link>
-        </li>
-      </ul>
-      <ul className="outside_links">
-        <li>
-          <a href="https://twitter.com">
-            <FaTwitter />
-          </a>
-        </li>
-        <li>
-          <a href="https://github.com/kubilaykandemir">
-            <FaGithub />
-          </a>
-        </li>
-        <li>
-          <a href="https://youtube.com">
-            <FaYoutube />
-          </a>
-        </li>
-        <li>
-          <a href="https://patreon.com">
-            <FaPatreon />
-          </a>
-        </li>
-      </ul>
-    </nav>
+      <nav>
+        <ul className="inside_links">
+          <li>
+            <Link to="/">Homepage</Link>
+          </li>
+          <li>
+            <Link to="/tags">Tags</Link>
+          </li>
+          <li>
+            <Link to="about">About</Link>
+          </li>
+        </ul>
+        <ul className="outside_links">
+          <li>
+            <a href="https://twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/kubilaykandemir">
+              <FaGithub />
+            </a>
+          </li>
+          <li>
+            <a href="https://youtube.com">
+              <FaYoutube />
+            </a>
+          </li>
+          <li>
+            <a href="https://patreon.com">
+              <FaPatreon />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
